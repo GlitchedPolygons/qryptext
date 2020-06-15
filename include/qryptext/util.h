a5e482f70fb61654980c28e86e1422cbc4e92918
Glitched Polygons <glitchedpolygons@protonmail.com>
2020-06-15 13:16:35 +0200

    Improved CMake inclusion of OQS and fixed a few util.h functions.

--- include/qryptext/util.h
+++ include/qryptext/util.h
@@ -21,17 +21,36 @@
 extern "C" {
 #endif
 
-#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
-#include <time.h>
-#include <kem_kyber.h>
+#include <stdlib.h>
+
+#ifdef _WIN32
+#define WIN32_NO_STATUS
+#include <windows.h>
+#undef WIN32_NO_STATUS
+#include <bcrypt.h>
+#endif
+
+#include <oqs/kem_kyber.h>
 #include <pqclean_kyber1024_clean/api.h>
+
+/**
+ * <c>x < y ? x : y</c>
+ */
+#define QRYPTEXT_MIN(x, y) (((x) < (y)) ? (x) : (y))
+
+/**
+ * <c>x > y ? x : y</c>
+ */
+#define QRYPTEXT_MAX(x, y) (((x) > (y)) ? (x) : (y))
 
 /**
  * Calculates the final output size of a ciphertext that would result from the qryptext_encrypt() function (based on a given plaintext length).
  * @param plaintext_length The amount of bytes to encrypt.
- * @return The final output size of the ciphertext if you encrypt data that is plaintext_length long with qryptext_encrypt().
+ * @return The final output size of the ciphertext if you encrypt data that is plaintext_length bytes long with qryptext_encrypt().
  */
 static inline size_t qryptext_calc_ciphertext_length(const size_t plaintext_length)
 {
@@ -39,15 +58,38 @@ static inline size_t qryptext_calc_ciphertext_length(const size_t plaintext_leng
 }
 
 /**
- * Gets a random 12-digit integer (ergo between <c>100000000000</c> and <c>999999999999</c>).
- * @return Random 12-digit integer
+ * (Tries to) read from <c>/dev/urandom</c> (or Windows equivalent, yeah...) filling the given \p output_buffer with \p output_buffer_size random bytes.
+ * @param output_buffer Where to write the random bytes into.
+ * @param output_buffer_size How many random bytes to write into \p output_buffer
  */
-static inline uint64_t get_random_big_int()
+static inline void qryptext_dev_urandom(uint8_t* output_buffer, const size_t output_buffer_size)
+{
+    if (output_buffer != NULL && output_buffer_size > 0)
+    {
+#ifdef _WIN32
+        BCryptGenRandom(NULL, output_buffer, output_buffer_size, BCRYPT_USE_SYSTEM_PREFERRED_RNG);
+#else
+        FILE* rnd = fopen("/dev/urandom", "r");
+        if (rnd != NULL)
+        {
+            fread(output_buffer, sizeof(uint8_t), output_buffer_size, rnd);
+            fclose(rnd);
+        }
+#endif
+    }
+}
+
+/**
+ * Gets a random big integer. This only features very limited randomness due to usage of <c>rand()</c>! <p>
+ * **DO NOT USE THIS FOR ANY TYPE OF KEY GENERATION!** <p>
+ * Current usage is for adding some lightweight additional entropy to the MbedTLS mbedtls_ctr_drbg_seed() function,
+ * which only gives the advantage of having a slightly different per-app starting point for the seed (as stated in the MbedTLS documentation).
+ * @return Random big number
+ */
+static inline unsigned long long int qryptext_get_random_big_integer()
 {
     srand(time(NULL) * time(NULL));
-    const uint64_t min = 100000000000;
-    const uint64_t max = 999999999999;
-    return min + rand() / (RAND_MAX / (max - min + 1) + 1);
+    return rand() * rand() * rand() * rand();
 }
 
 #ifdef __cplusplus
--- src/encrypt.c
+++ src/encrypt.c
@@ -50,18 +50,18 @@ int qryptext_encrypt(const uint8_t* data, const size_t data_length, uint8_t* out
     mbedtls_entropy_context entropy;
     mbedtls_ctr_drbg_context ctr_drbg;
 
-    char pers[32];
-    memset(pers, 0x00, sizeof(pers));
+    unsigned char pers[256];
+    qryptext_dev_urandom(pers, 128);
+    snprintf((char*)(pers + 128), 128, "qryptext_#!-$\\\"/.+@3#0%llu", qryptext_get_random_big_integer());
 
-    unsigned char aes256key[64];
+    unsigned char aes256key[32];
     memset(aes256key, 0x00, sizeof(aes256key));
 
     mbedtls_pk_init(&pk);
     mbedtls_entropy_init(&entropy);
     mbedtls_ctr_drbg_init(&ctr_drbg);
 
-    snprintf(pers, sizeof(pers), "qryptext_#!-$/.+@3#0%llu", get_random_big_int());
-    ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, (const unsigned char*)pers, sizeof pers);
+    ret = mbedtls_ctr_drbg_seed(&ctr_drbg, mbedtls_entropy_func, &entropy, pers, QRYPTEXT_MIN(sizeof(pers), (MBEDTLS_CTR_DRBG_MAX_SEED_INPUT - MBEDTLS_CTR_DRBG_ENTROPY_LEN - 1)));
     if (ret != 0)
     {
         mbedtls_printf(" failed\n  ! mbedtls_ctr_drbg_seed returned %d\n", ret);
